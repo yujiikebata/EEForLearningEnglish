018b90bd7402b01a83cfec4fd952e2e07644e013
yujiikebata <yujiikebata@gmail.com>
2016-02-24 22:17:28 +0900

    detect user action on browserAction

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     var activeTab = tabs[0];
-    chrome.tebs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+    chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
   });
 });
