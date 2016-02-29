500b8b2c8db3f458d69503acf6222fab87388479
yujiikebata <yujiikebata@gmail.com>
2016-02-29 22:45:07 +0900

    replace browserAction with contextMenus

--- background.js
+++ background.js
@@ -1,8 +1,13 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
-  });
+chrome.contextMenus.create({
+  "title": "LookUpInOALD",
+  "type": "normal", 
+  "contexts": ["selection"],
+  "onclick":function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+    });
+  }
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
